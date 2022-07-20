edbf305f8f6642736bd5cf8f9cb52737602d3ba8
GoncharovMain <goncharov.safe@gmail.com>
2022-07-20 08:45:23 +0300

    #1 variant.

--- query.sql
+++ query.sql
@@ -1,5 +1,7 @@
 
-select Products.Name, Categories.Name
+select Products.Name as Product,
+    case ISNULL(ProductCategory.FK_Category, -1) when -1 then '' else Categories.Name end as Category
 from ProductCategory, Products, Categories
-where Products.Id = Categories.Id
+where Products.Id = ProductCategory.FK_Product
+and Categories.Id = ISNULL(ProductCategory.FK_Category, 0)
 
